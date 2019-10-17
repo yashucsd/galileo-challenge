49a38ba7be98f174dfadae9011bfc730e6ec467e
yashucsd <yashplorer@gmail.com>
2019-10-17 15:45:06 -0700

    Adds documentation

--- __init__.py
+++ __init__.py
@@ -4,17 +4,20 @@ import requests, json
 
 app = Flask(__name__)
 
+# since this is a single page applicaiton, all routing is done thorugh the index
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	form = AddProvider(request.form)
-	displayed = request.cookies.get('displayed') # the doctors that are already displayed
+	displayed = request.cookies.get('displayed') # cookie stores doctors that are already displayed
 	docInfo = getDocInfo()
 
-	if displayed:
+	if displayed: # translates info from cookie
 		displayed = json.loads(displayed)
 
+	# eliminates displayed doctors from choices to add
 	updateChoices(form, displayed, docInfo)
 
+	# handles the cases of a form submission (add/clear provider(s))
 	if request.method == 'POST' and form.validate():
 		if request.form['submit'] == 'Clear providers':
 			displayed = None
@@ -25,17 +28,20 @@ def index():
 				displayed[doctor] = True
 			else:
 				displayed = {doctor: True}
+
+		# removes displayed doctor form choices
 		updateChoices(form, displayed, docInfo)
 
 	tasks = {}
-	if displayed:
+	if displayed: # prepares the list of tasks for each doctor displayed
 		updateTasks(tasks, displayed)
 
 	response = make_response(render_template('doctors.html', form=form, displayed=displayed, tasks=tasks, docInfo=docInfo))
-	response.set_cookie('displayed', json.dumps(displayed))
+	response.set_cookie('displayed', json.dumps(displayed)) # stores the displayed doctors in a cookie
 
 	return response
 
+# stores doctor info from API into a dictionary accessible by doctor ID
 def getDocInfo():
 	data = json.loads(requests.get('https://testapi.io/api/akirayoglu/0/reference/getDoctors').text)
 	docInfo = {}
@@ -45,8 +51,8 @@ def getDocInfo():
 
 	return docInfo
 
+# updates the doctors that can be added based on those displayed
 def updateChoices(form, displayed, data):
-
 	form.doctor.choices = []
 
 	for d_id in data:
@@ -57,6 +63,7 @@ def updateChoices(form, displayed, data):
 	if len(form.doctor.choices) > 0:
 		form.doctor.default = form.doctor.choices[0][0]
 
+# prepares and sorts the list of tasks for each doctor accessible by doctor ID
 def updateTasks(tasks, displayed):
 	for doctor in displayed.keys():
 		path = 'https://testapi.io/api/akirayoglu/0/tasks/' + doctor
@@ -65,8 +72,7 @@ def updateTasks(tasks, displayed):
 			continue
 		tasks[doctor].sort(key=lambda t: t['priority'], reverse=True)
 
+# required class to handle buttons in view
 class AddProvider(Form):
-    """Form to add provider to view."""
-
     doctor = SelectField('Doctor', validators=[validators.optional()])
     submit = SubmitField()
